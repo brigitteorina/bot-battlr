cf8babd9bdf38a8fee45902d30f121f172a0a2fd
brigitteorina <brigitteorina@gmail.com>
2023-04-30 00:02:33 +0300

    Red button works

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -43,7 +43,10 @@ function BotCard({ bot , clickEvent , deleteBot}) {
           <span>
             <div className="ui center aligned segment basic">
               <button
-               
+                className="ui mini red button"
+                onClick={(event) =>{
+                  event.stopPropagation();
+                  deleteBot(bot);
                   }
                 }
               >
