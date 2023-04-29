40a620f65a3fdb7cbb2741360fbc06a2423d10f1
brigitteorina <brigitteorina@gmail.com>
2023-04-30 00:08:54 +0300

    Bot collections

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -5,15 +5,15 @@ function BotCollection({bots, enlistBot, deleteBot}) {
   // Your code here
 
   const mapBots = bots.map(bot => 
-    <
+    <BotCard 
+    key={bot.id}
+    bot={bot}
+    clickEvent={enlistBot}
+    deleteBot={deleteBot}
     />)
 
   return (
-    <div className="ui four column grid">
-      <div className="row">
-        {mapBots}
-        Collection of all bots
-      </div>
+   
     </div>
   );
 }
