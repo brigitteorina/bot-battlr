f51b41e0d6e3b91842bcbe699fabbd7faeec7645
brigitteorina <brigitteorina@gmail.com>
2023-04-30 00:08:00 +0300

    Map bots

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -5,11 +5,7 @@ function BotCollection({bots, enlistBot, deleteBot}) {
   // Your code here
 
   const mapBots = bots.map(bot => 
-    <BotCard 
-    key={bot.id}
-    bot={bot}
-    clickEvent={enlistBot}
-    deleteBot={deleteBot}
+    <
     />)
 
   return (
